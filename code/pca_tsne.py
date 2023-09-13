3a2372513734e9d779d65c0f5aee8f1a378ce332
ansost <anste145@hhu.de>
2023-09-13 09:08:25 +0200

    Adds more doc in pca/tsne script

--- code/pca_tsne.py
+++ code/pca_tsne.py
@@ -1,13 +1,14 @@
 """Reduce dimensions for word embeddings using PCA and t-SNE.
-The 'conditions' variable specifies the number of dimensions/components 
-and the reduction algorithm (PCA or PCA + t-SNE). The filename also
-indicates the algorithm and the number of dimensions/components used. 
+The 'conditions' variable specifies the reduction algorithm 
+(PCA or PCA + t-SNE) and the number of dimensions/components. 
+The filename also indicates the algorithm and the number of 
+dimensions/components used. 
 
 Example:
 'pca_300' - PCA with 300 components
 'pcatsne_300_2' - PCA with 300 components followed by t-SNE with 2 dimensions
 """ 
-from typing import List, Tuple
+from typing import Tuple
 import numpy as np
 from tqdm import tqdm
 from sklearn.decomposition import PCA
@@ -51,22 +52,31 @@ def run_tsne(vectors: np.array, dims: int) -> np.array:
     result = tsne.fit_transform(vectors)
     return result
 
-def get_params(combination: str) -> Tuple(str, str, str):
+def get_params(combination: str) -> Tuple[str, str, str]:
+    """Get parameter combinations from str filename."""
     params = combination.split('_')
     algo = params[0]
-    components = params[1]
+    components = int(params[1])
     dims = None
     if len(params) == 3:
-        dims = params[2]
+        dims = int(params[2])
     return algo, components, dims
 
-def valid_algo(algo):
-    if algo != 'pca' or algo != 'pcatsne': 
-        print(f"Invalid algorithm: {combination}. Must be 'pca' or 'pcatsne'.") 
+def valid_algo(algo: str) -> bool():
+    """Check whether str filename contains valid algorithm (combination).
+    Current valid algorithms are either 'pca' or 'pcatsne'.
+    """
+    if str(algo) != 'pca' and str(algo) != 'pcatsne': 
+        print(f"Invalid algorithm: {algo}. Must be 'pca' or 'pcatsne'.") 
         return False
     return True
 
 if __name__ == '__main__':
+    # Combinations were chosen according to the analysis in finding_n-components.ipynb in the notebooks/ folder.
+    # the splits approximately represent the percentage of explained variance
+    # 300 components (100%), 200 components (90%), 80 components (75%)
+    # Since t-SNE does not have the same range of dimensions, all fo the PCA variations are ran with 
+    # t-SNE with 2 and three dimensions.    
     conditions = [
     'pca_300', 
     'pca_200', 
@@ -79,15 +89,16 @@ if __name__ == '__main__':
     'pcatsne_80_3'
     ]
 
-    word_embeddings = np.load("../../data/nonsynchr_casevectors.npy")
+    word_embeddings = np.load("../data/nonsynchr_casevectors.npy")
 
     print('Reducing data for combination...')
     for combination in tqdm(conditions): 
         algo, components, dims = get_params(combination)
+        print(combination)
         if valid_algo(algo):
             results = run_pca(vectors = word_embeddings, components = components)
         
             if algo == 'pcatsne':
                 if dims:
-                    results = run_tsne(vectors = results, tsne_dims = dims)
-            np.save("../../data/dim_reduced/nonsynchr_" + combination + ".npy", results)
+                    results = run_tsne(vectors = results, dims = dims)
+            np.save("../data/dim_reduced_input/nonsynchr_" + combination + ".npy", results)
