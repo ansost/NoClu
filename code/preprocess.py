153812573466f105a3091356b8cef64674759a15
ansost <anste145@hhu.de>
2023-09-14 11:24:15 +0200

    black formatting

--- code/preprocess.py
+++ code/preprocess.py
@@ -3,15 +3,15 @@ from sklearn.cluster import KMeans
 import fasttext as ft
 import numpy as np
 import time
-import json
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_time = time.time()
 
-    print('Loading data...')
+    print("Loading data...")
     list_of_all_forms = np.loadtxt(
-        "../data/list_of_noun_forms_full.csv", dtype=object, delimiter=",")
-    list_forms = list_of_all_forms[:, 4:] # List of cases without lemma and tags.
+        "../data/list_of_noun_forms_full.csv", dtype=object, delimiter=","
+    )
+    list_forms = list_of_all_forms[:, 4:]  # List of cases without lemma and tags.
 
     # Binary coding of synchretisms. A form at index i,j is not syncretic if matrix_of_syncr[i][j]==2**j.
     matrix_of_syncr = np.loadtxt(
@@ -21,15 +21,17 @@ if __name__ == '__main__':
     # Load fasttext model.
     ft = ft.load_model("../data/final-model.bin")
 
-    print('Retrieving non-synchratic case vectors from model...')
-    # Get case vectors for all non-synchretic cases. 
+    print("Retrieving non-synchratic case vectors from model...")
+    # Get case vectors for all non-synchretic cases.
     nonsynchr_casevectors = []
-    for form in range(0,len(list_forms)):
+    for form in range(0, len(list_forms)):
         for case in range(0, 14):
-            if matrix_of_syncr[form][case]==2**case: # Get word vector if case is not synchretic.
+            if (
+                matrix_of_syncr[form][case] == 2**case
+            ):  # Get word vector if case is not synchretic.
                 vector = ft.get_word_vector(list_forms[form][case])
                 nonsynchr_casevectors.append(vector)
 
     # Save the case vectors.
-    np.save('../data/nonsynchr_casevectors', nonsynchr_casevectors)
+    np.save("../data/nonsynchr_casevectors.npy", nonsynchr_casevectors)
     print(f"Preprocessing done! \nCompleted in {time.time() - start_time}.")
