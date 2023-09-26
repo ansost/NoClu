88276197872107568942c9f891251689f6212751
ansost <anste145@hhu.de>
2023-09-26 14:58:05 +0200

    remove old script

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,8 +1,9 @@
-"""Future complete preprocessing script for the project. For now only filters the synchretic cases."""
-from sklearn.cluster import KMeans
+"""Filter synchratic labels and retrieve nonsynchratic case vectors from fasttext model."""
+import time
+import pickle
 import fasttext as ft
 import numpy as np
-import time
+
 
 if __name__ == "__main__":
     start_time = time.time()
@@ -11,7 +12,9 @@ if __name__ == "__main__":
     list_of_all_forms = np.loadtxt(
         "../data/list_of_noun_forms_full.csv", dtype=object, delimiter=","
     )
-    list_forms = list_of_all_forms[:, 4:]  # List of cases without lemma and tags.
+
+    # List of cases without lemma and tags.
+    list_forms = list_of_all_forms[:, 4:]
 
     # Binary coding of synchretisms. A form at index i,j is not syncretic if matrix_of_syncr[i][j]==2**j.
     matrix_of_syncr = np.loadtxt(
@@ -19,19 +22,32 @@ if __name__ == "__main__":
     ).astype(int)
 
     # Load fasttext model.
-    ft = ft.load_model("../data/final-model.bin")
+    model = ft.load_model("../data/final-model.bin")
 
-    print("Retrieving non-synchratic case vectors from model...")
+    print("Filtering synchretic cases from labels...")
     # Get case vectors for all non-synchretic cases.
-    nonsynchr_casevectors = []
+    nonsynchr_caselabels = []
     for form in range(0, len(list_forms)):
+        sublist = []
         for case in range(0, 14):
             if (
                 matrix_of_syncr[form][case] == 2**case
-            ):  # Get word vector if case is not synchretic.
-                vector = ft.get_word_vector(list_forms[form][case])
-                nonsynchr_casevectors.append(vector)
+            ):  # Get label if label is not synchretic.
+                label = list_forms[form][case]
+                sublist.append(label)
+        nonsynchr_caselabels.append(sublist)
 
-    # Save the case vectors.
-    np.save("../data/nonsynchr_casevectors.npy", nonsynchr_casevectors)
+    with open(
+        "../data/nonsynchr_labels.pkl", "wb"
+    ) as f:  # Can't save a jagged array with numpy.
+        pickle.dump(nonsynchr_caselabels, f)
+
+    print("Retrieving non-synchratic vectors from model...")
+    vectors = []
+    for sublist in nonsynchr_caselabels:
+        for label in sublist:
+            vector = model.get_word_vector(label)
+            vectors.append(vector)
+
+    np.save("../data/nonsynchr_casevectors_test.npy", vectors)
     print(f"Preprocessing done! \nCompleted in {time.time() - start_time}.")
