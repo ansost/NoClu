4741494bd7a45e3bce31eeeb663a8193ebee8dd4
ansost <anste145@hhu.de>
2023-10-01 19:58:14 +0200

    more documentation for eval module

--- code/evaluation.py
+++ code/evaluation.py
@@ -1,40 +1,27 @@
-"""Evaluate clustering algorithms using a range of common metrics."""
+"""
+Evaluate clustering algorithms using a maximum flow minimum cost algorithm.
+
+Exports the functions:
+- evaluate_clustering(): evaluate clustering algorithms using a range of common metrics.
+- translate_labels(): translate predicted labels back to original labels.
+- overlap(): compute overlap between a predicted cluster and a label form the gold labels.
+- mincostflow(): compute a min cost flow between the predicted and the true labels.
+"""
 
 from time import time
 from sklearn import metrics
 from numpy.typing import ArrayLike
 from typing import List, Dict
 import string
+import json
 from collections import Counter, defaultdict
 import networkx as nx
 from networkx.algorithms import bipartite
 
 
-# def evaluate_clusters(true_labels: ArrayLike, estimated_labels: ArrayLike) -> float:
-#     """Benchmark a clustering algorithm using a range of common metrics.
-
-#     Parameters:
-#     -----------
-#     name:
-#         Name of the clustering algorithm.
-#     data:
-#         The data to cluster.
-#     true_labels:
-#         The true labels of the data.
-#     estimated_labels:
-#         The labels estimated by the clustering algorithm.
-
-#     Returns:
-#     --------
-#     metrics:
-#         A dictionary of the metric names as keys and their values.
-#     """
-#     # The silhouette score requires the full dataset
-#     silhuette = metrics.silhouette_score(X=true_labels, labels=estimated_labels)
-#     return silhuette
-
-
-def translate_labels(labels: ArrayLike, gold_labels=ArrayLike) -> Dict[str, List[int]]:
+def translate_labels(
+    labels: ArrayLike, gold_labels=ArrayLike, from_dbscan: bool = None
+) -> Dict[str, List[int]]:
     """Translate predicted labels back to original labels.
     For each input word, add it to a dictionary with clusters as keys and the original labels as values.
 
@@ -51,9 +38,16 @@ def translate_labels(labels: ArrayLike, gold_labels=ArrayLike) -> Dict[str, List
         Dictionary of lists keyed by clusters such that clusters and their members' gold labels as values in a list such that: ['C0'] = [0, 1, 0, 3, 13, 13, 6, 7, 7, 7]
     """
     clusters = defaultdict(list)
-    for index, label in enumerate(labels):
-        actual_label = gold_labels[index]
-        clusters["C" + str(label)].append(actual_label)
+
+    if from_dbscan:
+        for index, label in enumerate(labels):
+            if label != -1:
+                actual_label = gold_labels[index]
+                clusters["C" + str(label)].append(actual_label)
+    else:
+        for index, label in enumerate(labels):
+            actual_label = gold_labels[index]
+            clusters["C" + str(label)].append(actual_label)
     return clusters
 
 
@@ -81,13 +75,18 @@ def overlap(cluster: List[int], label: int) -> int:
 
 
 def mincostflow(predicted_labels: ArrayLike) -> Dict:
-    """Compute a min cost flow between the predicted and the true labels.
-    Creates a directed digraph with edges between predicted and true labels.
+    """Compute a minimum cost maximum flow between the predicted and the true labels.
+    Creates a directed graph (Digraph) with edges between predicted and true labels. a super source and sink
+    between which to compute the flow. The cost of each edge is the negative overlap between the predicted
+    and the true label. The capacity of each edge is 1 or infinite if not set.
+
+    Predicted clusters are represented by numbers and gold labels are represented by numbers preceeded by a 'c'.
+    For the graph, bipartite = 0 is the gold labels and bipartite = 1 is the predicted labels.
 
     Parameters:
     -----------
     predicted_labels:
-        The labels estimated by the clustering algorithm.
+        The labels estimated by the clustering algorithm and translated to the original labels.
 
     Returns:
     --------
@@ -98,11 +97,9 @@ def mincostflow(predicted_labels: ArrayLike) -> Dict:
     """
 
     predictionclusters_as_nodes = [
-        string.ascii_lowercase[i] for i in range(len(predicted_labels.keys()))
+        "C" + str(i) for i in range(len(predicted_labels.keys()))
     ]
 
-    # Initialize all gold labels as nodes represented by numbers. Predicted clusters are letters.
-    # Bipartite = 0 is the gold labels and bipartite = 1 is the predicted labels.
     B = nx.DiGraph()
     B.add_nodes_from(list(range(0, 14)), bipartite=0)
     B.add_nodes_from(predictionclusters_as_nodes, bipartite=1)
@@ -114,7 +111,7 @@ def mincostflow(predicted_labels: ArrayLike) -> Dict:
             if cost:
                 B.add_edge(
                     label, predictionclusters_as_nodes[index], weight=cost, capacity=1
-                )  # TODO: Stimmt capacity=1 here?
+                )
 
     B.add_node("super_source")
     for node in range(0, 14):
@@ -122,9 +119,7 @@ def mincostflow(predicted_labels: ArrayLike) -> Dict:
 
     B.add_node("super_sink")
     for node in predictionclusters_as_nodes:
-        B.add_edge(
-            node, "super_sink", capacity=1
-        )  # If capacity is not set, it is assumed to be infinite.
+        B.add_edge(node, "super_sink", capacity=1)
 
     # Compute min cost flow.
     flowDict = nx.max_flow_min_cost(B, s="super_source", t="super_sink")
